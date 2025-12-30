bb0f78337549c33247a2bf1324865dd15adf1144
lochner <development@lochner.tech>
2025-12-30 10:00:58 -0600

    Revert "Merge branch 'alfe/2772'"
    This reverts commit d023beb2658570f2ed3bbd17d93f9c25b0eb9aa5.

--- tools/stage_changes.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-from pathlib import Path
-import sys
-from subprocess import check_call
-
-# Stage all changes in the workspace
-check_call(["git","add","-A"]) 
-print('staged')
