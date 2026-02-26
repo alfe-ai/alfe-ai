967258bc125f67fffd601af7d5d191ff58529122
lochner <development@lochner.tech>
2026-02-25 21:03:32 -0600

    Reapply "Merge branch 'alfe/4751'"
    This reverts commit c60a0280baec229ab341ecde1fe799b70648d3fd.

--- AlfeCode/public/JS/codex_runner.js
+++ AlfeCode/public/JS/codex_runner.js
@@ -5740,7 +5740,6 @@ const appendMergeChunk = (text, type = "output") => {
         || (typeof run.effectivePrompt === "string" && run.effectivePrompt)
         || "";
     if (promptInput && promptValue) {
-      promptInput.value = promptValue;
     }
     updatePromptPreview(promptValue);
 
